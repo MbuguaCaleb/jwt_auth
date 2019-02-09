be9fcf9f60aa4c2f243c3c44f4125bc26dcd8e37
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-02-09 16:37:24 +0300

    [finishes] all todos

--- run.py
+++ run.py
@@ -7,11 +7,11 @@ import datetime
 from functools import wraps
 
 
-
 app=Flask(__name__)
 app.config['SECRET_KEY']='this is secret'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db = SQLAlchemy(app)
+
 
 
 class User(db.Model):
@@ -20,6 +20,7 @@ class User(db.Model):
     name = db.Column(db.String(50))
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
+
 
 
 class Todo(db.Model):
@@ -168,6 +169,84 @@ def login():
         token = jwt.encode({'public_id':user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
         
         return jsonify({'token': token.decode('UTF-8')})
+
+
+@app.route('/todo', methods=['GET'])
+@token_required
+def get_all_todos(current_user):
+    todos = Todo.query.filter_by(user_id=current_user.id).all()
+
+    output = []
+    
+    for todo in todos:
+        todo_data = {}
+        todo_data['id']=todo.id
+        todo_data['text']=todo.text
+        todo_data['complete']=todo.complete
+        output.append(todo_data)
+        
+    return jsonify({'todos': output}) 
+
+
+@app.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+    data = request.get_json()
+
+    new_todo = Todo(text=data['text'],complete=False, user_id=current_user.id)
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify({'message':'Todo created!'})
+
+
+
+@app.route('/todo/<todo_id>', methods=['GET'])
+@token_required
+
+def get_one_todo(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id,user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message':'No todo found!'})
+    
+    
+    todo_data={}
+    todo_data['id']=todo.id
+    todo_data['text']=todo.text
+    todo_data['complete']=todo.complete
+
+
+    return jsonify(todo_data)
+
+
+@app.route('/todo/<todo_id>', methods=['PUT'])
+@token_required
+def complete_todo(current_user,todo_id):
+
+    todo = Todo.query.filter_by(id=todo_id,user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message':'No todo found!'})
+
+    todo.complete = True
+    db.session.commit()
+
+    return jsonify({'message':'Todo item has been completed!'})
+
+@app.route('/todo/<todo_id>', methods=['DELETE'])
+@token_required
+def delete_todo(current_user,todo_id):
+
+
+    todo = Todo.query.filter_by(id=todo_id,user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message':'No todo found!'})
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return jsonify({'message':'Todo item deleted!'})
 
 
 if __name__ =='__main__':
